195dca8a74927a59729a2a1f424c7998f14d05ab
Thembek <andile.goba@younglings.africa>
2024-10-07 09:45:29 +0200

    React navigation package commit

--- app/index.js
+++ app/index.js
@@ -1,14 +1,22 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
+
+const Stack = createNativeStackNavigator();
 
 const Home = () => {
     return(
         <>
             <StatusBar style='light'/>
-            <CategoriesScreen />
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name='MealsCategories' component={CategoriesScreen}/>
+                </Stack.Navigator>
+            </NavigationContainer>   
         </>
         
     )
