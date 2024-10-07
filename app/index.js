697c07ea2b7ef1b7986a7956d39c86320515d83f
Thembek <andile.goba@younglings.africa>
2024-10-07 09:29:26 +0200

    Displaying items in a grid with style commit

--- app/index.js
+++ app/index.js
@@ -1,11 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 
 const Home = () => {
     return(
-        <CategoriesScreen />
+        <>
+            <StatusBar style='light'/>
+            <CategoriesScreen />
+        </>
+        
     )
 }
 
