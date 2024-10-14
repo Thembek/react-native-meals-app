af49236dd78e62262b08caab57fc63eb264ba026
Thembek <andile.goba@younglings.africa>
2024-10-14 10:36:24 +0200

    drawer finishing touches commit

--- app/index.js
+++ app/index.js
@@ -3,7 +3,8 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createDrawerNavigator } from '@react-navigation/drawer'
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Ionicons } from '@expo/vector-icons';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import MealsOverviewScreen from '../screens/MealsOverviewScreen';
@@ -19,9 +20,21 @@ function DrawerNavigator(){
             headerStyle: { backgroundColor: '#351401' },
             headerTintColor: 'white',
             sceneContainerStyle: { backgroundColor: '#3f2f25' },
+            drawerContentStyle: { backgroundColor: '#351401' },
+            drawerInactiveTintColor: 'white',
+            drawerActiveTintColor: '#351401',
+            drawerActiveBackgroundColor: '#e4baa1',
+
         }}>
-            <Drawer.Screen name='Categories' component={CategoriesScreen} options={{ title: 'All Categories' }}/>
-            <Drawer.Screen name='' component={FavoritesScreen} />
+            <Drawer.Screen name='Categories' component={CategoriesScreen} options={{ 
+                title: 'All Categories', 
+                drawerIcon: ({ color, size }) => <Ionicons name='list' color={color} size={size}/>
+            }}/>
+            <Drawer.Screen name='' component={FavoritesScreen} options={{
+                drawerIcon: ({ color, size }) => (
+                    <Ionicons name='star' color={color} size={size}/>
+                )
+            }}/>
         </Drawer.Navigator>
     )
 }
