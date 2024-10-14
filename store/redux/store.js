7ddb81b460e867a8c27e412f407f23c7641d67be
Thembek <andile.goba@younglings.africa>
2024-10-14 12:03:46 +0200

    Working with redux slices commit

--- store/redux/store.js
+++ store/redux/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import favoritesReducer from './favorites'
+
 const store = configureStore({
-    reducer: {}
+    reducer: {
+        favoriteMeals: favoritesReducer;
+    }
 });
