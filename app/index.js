6a0a072105afcedfc3f54bebadb80e89bf6dce31
Thembek <andile.goba@younglings.africa>
2024-10-14 11:52:58 +0200

    getting started with redux & redux toolkit commit

--- app/index.js
+++ app/index.js
@@ -5,12 +5,14 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Ionicons } from '@expo/vector-icons';
+import { Provider } from 'react-redux';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import MealsOverviewScreen from '../screens/MealsOverviewScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
 import FavoritesScreen from '../screens/FavoritesScreen';
-import FavoriteContextProvider from '../store/context/favorites-context';
+//import FavoriteContextProvider from '../store/context/favorites-context';
+import { store } from '../store/redux/store'
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -43,7 +45,8 @@ const Home = () => {
     return(
         <>
             <StatusBar style='light'/>
-            <FavoriteContextProvider>
+            {/*<FavoritesContextProvider> */}
+            <Provider store={store}>
                 <NavigationContainer independent={true}>
                     <Stack.Navigator screenOptions={{
                         headerStyle: { backgroundColor: '#351401'},
@@ -59,7 +62,8 @@ const Home = () => {
                         }}/>
                     </Stack.Navigator>
                 </NavigationContainer>
-            </FavoriteContextProvider>   
+            </Provider>
+            {/*</FavoriteContextProvider>*/}   
         </>
         
     )
